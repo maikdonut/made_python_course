055784c6e6dc6672c7ce2a0fd5db44ffd9f793e7
maikdonut <73718190+maikdonut@users.noreply.github.com>
2022-09-13 16:49:32 +0300

    Quadratic equation

--- 01/1.py
+++ 01/1.py
@@ -1,15 +1,15 @@
 from math import sqrt
 
 def quad_eq(a,b,c):
-    assert a!=0, 'Not quadratic equation'
+    assert a != 0, 'Not quadratic equation'
     d = b**2 - 4*a*c
     if (d<0):
         print("No real roots")
     elif (d==0):
         print(f'Root is:{-b/(2*a)}')
     else:
-        print(f'First root is:{(-b+sqrt(d))/(2*a)}')
-        print(f'Second root is:{(-b-sqrt(d))/(2*a)}')
+        print(f'First root is:{(-b + sqrt(d))/(2*a)}')
+        print(f'Second root is:{(-b - sqrt(d))/(2*a)}')
 
 a = int(input('Enter a:'))  
 b = int(input('Enter b:'))  
