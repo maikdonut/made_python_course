e5e57d27eb68a2440c3bddf5b804326c36cfad31
maikdonut <73718190+maikdonut@users.noreply.github.com>
2022-09-13 18:16:26 +0300

    Update 1.py

--- 01/1.py
+++ 01/1.py
@@ -1,5 +1,3 @@
-from math import sqrt
-
 def quad_eq(a,b,c):
     assert a != 0, 'Not quadratic equation'
     d = b**2 - 4*a*c
@@ -8,8 +6,8 @@ def quad_eq(a,b,c):
     elif (d==0):
         print(f'Root is:{-b/(2*a)}')
     else:
-        print(f'First root is:{(-b + sqrt(d))/(2*a)}')
-        print(f'Second root is:{(-b - sqrt(d))/(2*a)}')
+        print(f'First root is:{(-b + d**0.5)/(2*a)}')
+        print(f'Second root is:{(-b - d**0.5)/(2*a)}')
 
 a = int(input('Enter a:'))  
 b = int(input('Enter b:'))  
