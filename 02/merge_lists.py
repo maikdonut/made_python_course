f1eaba6edc1e6c145c48ebf9427beaefba7024f2
maikdonut <73718190+maikdonut@users.noreply.github.com>
2022-09-19 12:24:26 +0300

    Add sorted() to merge function

--- 02/merge_lists.py
+++ 02/merge_lists.py
@@ -1,7 +1,7 @@
 def merge(seq_1,seq_2):
     return sorted(set(seq_1) & set(seq_2))
 
-assert merge([1, 1, 2, 5, 7], [1, 1, 2, 3, 4, 7]) == [1, 2, 7]
+assert merge([-4, -2, -2, 0, 3, 5], [-2, 0, 0, 3, 7, 10]) == [-2, 0, 3]
 assert merge([-2, -2, -2, -2], []) == []
 assert merge([1, 1], [1, 1, 1, 1]) == [1]
 assert merge([2, 4, 6, 8], [1, 3, 5, 7]) == []
