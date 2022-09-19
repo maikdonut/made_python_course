ea5444ae7cbc48c0538137d62da2a22864a02939
maikdonut <73718190+maikdonut@users.noreply.github.com>
2022-09-19 11:56:52 +0300

    Update closest_zero function

--- 02/closest_to_zero.py
+++ 02/closest_to_zero.py
@@ -1,23 +1,15 @@
 def closest_zero(nums): 
-    nums = list(sorted(set(nums)))
     assert len(nums) != 0, "Empty list"
-    neg = [i for i in nums if i < 0]
-    pos = nums[len(neg):]
-    
-    if len(neg) == 0:
-        return [pos[0]]
-    elif len(pos) == 0:
-        return [neg[-1]]
-    else:
-        if abs(neg[-1]) == pos[0]:
-            return [neg[-1], pos[0]]
-        elif abs(neg[-1]) > pos[0]:
-            return [pos[0]]
-        else:
-            return [neg[-1]]
+    pos_nums = [abs(i) for i in nums]
+    close = min(pos_nums)
+    result = []
+    for i in range(len(nums)):
+        if pos_nums[i] == close:
+            result.append(nums[i])
+    return result
         
-assert closest_zero([-5, 9, 6, -8]) == [-5]
-assert closest_zero([1, 2, -5, -1]) == [-1, 1]
+assert closest_zero([1, -1, 1, -1, 1, 1]) == [1, -1, 1, -1, 1, 1]
+assert closest_zero([2, 2, -5, 9, 3, -2]) == [2, 2, -2]
 assert closest_zero([-5, -10, -2, -5]) == [-2]
 assert closest_zero([4, 1, 3, 2, 5]) == [1]
 assert closest_zero([6, -1, 1, 3, 0]) == [0]
