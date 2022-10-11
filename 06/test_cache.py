a7cbba868e8ed9b41a733c50b8a92d2af42635e4
maikdonut <73718190+maikdonut@users.noreply.github.com>
2022-10-11 11:52:01 +0300

    Update test_cache.py

--- 06/test_cache.py
+++ 06/test_cache.py
@@ -5,24 +5,24 @@ from cache import LRUCache
 
 class TestLRUCache(unittest.TestCase):
     def test_set_value(self):
-        self.cache = LRUCache(2)
-        self.cache.set_value("k2", "val2")
-        self.cache.set_value("k1", "val1")
-        self.assertEqual(self.cache.queue, deque(["k2", "k1"]))
-        self.assertEqual(self.cache.dict, {"k2": "val2", "k1": "val1"})
-        self.cache.set_value("k3", "val3")
-        self.assertEqual(self.cache.queue, deque(["k1", "k3"]))
-        self.assertEqual(self.cache.dict, {"k3": "val3", "k1": "val1"})
+        cache = LRUCache(2)
+        cache.set_value("k2", "val2")
+        cache.set_value("k1", "val1")
+        self.assertEqual(cache.queue, deque(["k2", "k1"]))
+        self.assertEqual(cache.dict, {"k2": "val2", "k1": "val1"})
+        cache.set_value("k3", "val3")
+        self.assertEqual(cache.queue, deque(["k1", "k3"]))
+        self.assertEqual(cache.dict, {"k3": "val3", "k1": "val1"})
 
     def test_get_value(self):
-        self.cache = LRUCache(3)
-        self.cache.set_value("k2", "val2")
-        self.cache.set_value("k1", "val1")
-        self.cache.set_value("k3", "val3")
-        res = self.cache.get_value("k1")
+        cache = LRUCache(3)
+        cache.set_value("k2", "val2")
+        cache.set_value("k1", "val1")
+        cache.set_value("k3", "val3")
+        res = cache.get_value("k1")
         self.assertEqual(res, "val1")
-        self.assertEqual(self.cache.queue, deque(["k2", "k3", "k1"]))
-        res = self.cache.get_value("k4")
+        self.assertEqual(cache.queue, deque(["k2", "k3", "k1"]))
+        res = cache.get_value("k4")
         self.assertEqual(res, None)
 
 
